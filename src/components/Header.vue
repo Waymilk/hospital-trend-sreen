a49ee2eaf1dee3d1e596d64da1bec6b34117b737
Waymilk <https://Waymilk@github.com>
2020-09-08 15:34:16 +0800

    右上角时间

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -23,17 +23,41 @@
         </div>
       </div>
     </div>
+    <div class="now-date">
+      <div>{{weekday}}</div>
+      <div>{{nowDate}} </div>
+    </div>
+    <div class="now-time">
+      {{nowTime}}
+    </div>
   </div>
 </template>
 
 <script>
+import { formatDate } from '../utils/common'
 export default {
   name: 'ScreenHeader',
   data() {
+    const weekArr = ["日","一","二","三","四","五","六"]
     return {
+      timer:null,
       title: this.$route.meta.title,
-      enTitle: this.$route.meta.enTitle
+      enTitle: this.$route.meta.enTitle,
+      weekArr:["日","一","二","三","四","五","六"],
+      nowDate:formatDate(new Date(),'yyyy年MM月dd日'),
+      nowTime:formatDate(new Date(),'HH:mm:ss'),
+      weekday:`星期${weekArr[new Date().getDay()]}`,
     }
+  },
+  created(){
+    this.timer = setInterval(() => {
+      this.weekday = `星期${this.weekArr[new Date().getDay()]}`
+      this.nowDate = formatDate(new Date(),'yyyy年MM月dd日')
+      this.nowTime = formatDate(new Date(),'HH:mm:ss')
+    }, 1000);
+  },
+  beforeDestroy(){
+    clearInterval(this.timer)
   }
 }
 </script>
@@ -80,6 +104,20 @@ export default {
     .eng{
       font-size: 14px;
     }
+  }
+  .now-date{
+    position: absolute;
+    text-align: right;
+    line-height: 1.4;
+    right: 20px;
+    font-size: 12px;
+    top:24px;
+  }
+  .now-time{
+    position: absolute;
+    right: 140px;
+    top: 26px;
+    font-size: 26px;
   }
 }
 </style>
